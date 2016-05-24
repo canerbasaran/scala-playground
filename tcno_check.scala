ae9bcd1039f66afb02ccc87f98e18dd45b23c983
Caner Başaran <basaran.caner@gmail.com>
2016-05-24 05:06:48 +0300

    Update tcno_check.scala

--- tcno_check.scala
+++ tcno_check.scala
@@ -1,4 +1,4 @@
-val select = (list:Seq[Int], start:Int, end:Int, step:Int) => for (i <- (start until end by step).toList) yield list(i)
+val select = (list:Seq[Int], start:Int, end:Int, step:Int) => for (i <- start until end by step) yield list(i)
 def check(tc_no: String): Boolean = {
     val list_tc = tc_no.map(_.asDigit)
     val tc10 = (select(list_tc, 0, 10, 2).sum*7 - select(list_tc, 1, 9, 2).sum) % 10
